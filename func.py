ca8fed1b0455402cc5631f35dbc41864488a2e5f
Hiroki1112 <pystdy@yahoo.co.jp>
2020-02-15 20:18:14 +0900

    2/15

--- func.py
+++ func.py
@@ -4,8 +4,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pathlib
 
-def trimmingData(df):
-    #株価が-の時は前日終値を値として使う
-    df.loc[df["株価"]=='-',"株価"] = df.loc[df["株価"]=='-',"前日終値"]
+spPrefix = 'japan-all-stock-prices-2_'
+siPrefix = 'japan-all-stock-data_'
+finPrefix = 'japan-all-stock-financial-results_'
 
+def makedir(dirName):
+    #IPOもあるので、毎回確認する。
+    #ファイルはdataディレクトリ下に保存
+    os.makedirs('data/'+dirName,exist_ok=True)
 
+def getFileName(dirName):
+    #引数で受け取ったディレクトリ下にあるファイル名を返す関数
+    #引数のディレクトリ下のファイル名のみリストで返す
+    files = os.listdir(dirName)
+    return [file for file in files if os.path.isfile(os.path.join(dirName, file))]
