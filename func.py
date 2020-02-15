3e3c9e43900c948599896253415b1aafbbcfc9c2
Hiroki1112 <pystdy@yahoo.co.jp>
2020-02-15 21:34:54 +0900

    関数追加

--- func.py
+++ func.py
@@ -6,7 +6,9 @@ import pathlib
 
 spPrefix = 'japan-all-stock-prices-2_'
 siPrefix = 'japan-all-stock-data_'
+sinPrefix = 'japan-all-stock-margin-transactions'
 finPrefix = 'japan-all-stock-financial-results_'
+prefixList = [spPrefix,siPrefix,finPrefix,sinPrefix]
 
 def makedir(dirName):
     #IPOもあるので、毎回確認する。
@@ -18,3 +20,21 @@ def getFileName(dirName):
     #引数のディレクトリ下のファイル名のみリストで返す
     files = os.listdir(dirName)
     return [file for file in files if os.path.isfile(os.path.join(dirName, file))]
+
+def checkFiles(fileNames):
+    #4ファイルを使用する。別のファイルが入っていたらエラーを出す。
+    files = []
+    for f in fileNames:
+        for pre in prefixList:
+            if pre in f:
+                files.append(f)
+    
+    if len(files) != 4:
+        print('◆◇'*20)
+        print('ERROR')
+        print('ファイル数が不正です。\ndailyファイルを確認してください。')
+        print('◆◇'*20)
+    
+    #print(files)
+    return file
+
